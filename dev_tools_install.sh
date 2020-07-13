09d614eafc6fd04d358ef1afeb9fa9af1b7a7b1f
orestispanago <orestispanago@gmail.com>
2020-07-13 18:11:53 +0300

    Added commands to install docker-compose and Opera browser .deb

--- dev_tools_install.sh
+++ dev_tools_install.sh
@@ -5,6 +5,10 @@ sudo sh get-docker.sh
 sudo usermod -aG docker $USER
 rm get-docker.sh
 
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
@@ -20,13 +24,14 @@ flatpak install flathub org.gnome.DejaDup -y
 
 
 wget https://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community_8.0.20-1ubuntu18.04_amd64.deb
+wget https://download3.operacdn.com/pub/opera/desktop/69.0.3686.57/linux/opera-stable_69.0.3686.57_amd64.deb
 sudo dpkg -i *.deb
 sudo apt-get install -f -y
 rm mysql-workbench-community_8.0.20-1ubuntu18.04_amd64.deb
-
-
+#
+#
 # Install jdk if not present on system
-# sudo apt-get install -y openjdk-8-jdk
+ sudo apt-get install -y openjdk-8-jdk
 
 # Run netbeans installer only if it does not work from home folder restore
 # wget http://download.netbeans.org/netbeans/8.2/rc/bundles/netbeans-8.2-javaee-linux.sh
