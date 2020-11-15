3c2596c156bdace0bc328239d6388b14ac00d3df
orestispanago <orestispanago@gmail.com>
2020-11-15 03:25:07 +0200

    Added brave browser

--- dev_tools_install.sh
+++ dev_tools_install.sh
@@ -48,3 +48,10 @@ rm *.deb
 # Run netbeans installer only if it does not work from home folder restore
 # wget http://download.netbeans.org/netbeans/8.2/rc/bundles/netbeans-8.2-javaee-linux.sh
 # sudo sh netbeans-8.2-javaee-linux.sh
+
+# Brave browser 
+sudo apt install apt-transport-https curl gnupg
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser -y
