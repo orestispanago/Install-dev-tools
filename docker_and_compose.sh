bf1459e031b93294ce86141d2f13797a9f49c92b
Orestis Panagopoulos <orestispanago@gmail.com>
2021-03-19 19:58:50 +0200

    Updated with latest version of docker-compose

--- docker_and_compose.sh
+++ docker_and_compose.sh
@@ -7,6 +7,6 @@ sudo usermod -aG docker $USER
 rm get-docker.sh
 
 # docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
